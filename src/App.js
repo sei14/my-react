d3d63b72f05699b74897b86350aa881a44a06a35
yamashitasei <sei1434@icloud.com>
2023-05-12 15:34:55 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import React from 'react';
-//import Fetch from './Fetch';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import page1 from './page1'; //作成したpage1.jsを読み込んでいる
+import Fetch from './Fetch';
 
 function App() {
   return (
-    /*<div className="App">
+    <div className="App">
       <header className="App-header">
         <h1>掲示板</h1>
         <p>スレッドをたてる</p>
@@ -17,12 +15,9 @@ function App() {
         <h2>新着スレッド</h2>
       <Fetch className='list-api'/>
       </main>
-    </div>*/
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={page1} />　//URLのパスが'/'のみの時にpage1を表示する
-    </Switch>
-  </BrowserRouter>
+    </div>
+    
+   
   );
 }
 
