2ed514826843a2236f11d845ac20154f89f878d2
yamashitasei <sei1434@icloud.com>
2023-05-17 21:18:10 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 
 // react-router-domのインポートを追加
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import { Home } from "./components/Home";
-import { Page1 } from "./components/Page1";
-
+import Home from "./components/Home";
+import Page1 from "./components/Page1";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/page1" component={Page1} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/page1" component={Page1} />
       </div>
     </Router>
-   
   );
 }
-function Home() {
-  return <h1>Home</h1>;
-}
-function Page1() {
-  return <h1>Page 1</h1>;
-}
-
 
 export default App;
 
