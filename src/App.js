c9007dbb9906a4eafbc6cc0d63509b6c8befa759
yamashitasei <sei1434@icloud.com>
2023-05-17 21:11:51 +0900

    comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
-import React from 'react';
-import Fetch from './Fetch';
+
+// react-router-domのインポートを追加
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Home } from "./components/Home";
+import { Page1 } from "./components/Page1";
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+      <Route exact path="/" component={Home} />
+      <Route exact path="/page1" component={Page1} />
+      </div>
+    </Router>
+   
+  );
+}
+function Home() {
+  return <h1>Home</h1>;
+}
+function Page1() {
+  return <h1>Page 1</h1>;
+}
+
+
+export default App;
+
+
+
+   /* <div className="App">
       <header className="App-header">
         <h1>掲示板</h1>
         <p>スレッドをたてる</p>
@@ -15,10 +40,7 @@ function App() {
         <h2>新着スレッド</h2>
       <Fetch className='list-api'/>
       </main>
-    </div>
+    </div>*/
+     
     
    
-  );
-}
-
-export default App;
